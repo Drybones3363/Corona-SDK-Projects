8ddc7a51590a44440cb31b44557998c30e3b7038
Adam Galauner <adam.galauner@yahoo.com>
2016-06-23 21:44:47 -0400

    Create config.lua

---
+++ GetThatBoy/config.lua
@@ -0,0 +1,24 @@
+application =
+{
+	--[[window =
+	{
+		defaultViewWidth = 500,
+		defaultViewHeight = 500,
+		resizable = false,
+		enableCloseButton = true,
+		enableMinimizeButton = true,
+		showWindowTitle = true,
+		titleText = {
+			default = "Get that Boy *no meme pun intended*"
+		},
+	
+	},
+	--]]
+	content =
+	{
+		width = 500,
+		height = 500, 
+		scale = "letterBox",
+		fps = 30
+	}
+}
